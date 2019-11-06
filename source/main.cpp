3b576d6adb74fb703d1c8cfd35b434b4183e10c9
kjkaminsk <krzysztof.kaminski.j@gmail.com>
2019-11-06 22:09:25 +0100

    Submit command buffer in draw()

--- source/main.cpp
+++ source/main.cpp
@@ -7,21 +7,36 @@
 #include <vector>
 
 #include "context.h"
+#include "errors.h"
 #include "window.h"
 #include "graphics_pass.h"
 
-void draw()
+void draw(Context& ctx, Graphics_Pass& pass)
 {
+    uint32_t image_index;
+    vkAcquireNextImageKHR(ctx.device, ctx.swap_chain, UINT64_MAX, ctx.image_acquired, nullptr, &image_index);
 
+    VkSemaphore waitSemaphores[] = { ctx.image_acquired };
+    VkPipelineStageFlags waitStages[] = { VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT };
+    VkSubmitInfo submitInfo = { VK_STRUCTURE_TYPE_SUBMIT_INFO };
+    submitInfo.waitSemaphoreCount = 1;
+    submitInfo.pWaitSemaphores = waitSemaphores;
+    submitInfo.pWaitDstStageMask = waitStages;
+    submitInfo.commandBufferCount = 1;
+    submitInfo.pCommandBuffers = &pass.cmd_buffers[image_index];
+    VkSemaphore signalSemaphores[] = { ctx.rendering_complete };
+    submitInfo.signalSemaphoreCount = 1;
+    submitInfo.pSignalSemaphores = signalSemaphores;
+    tif(FL, vkQueueSubmit(ctx.queue, 1, &submitInfo, nullptr));
 }
 
-void main_loop(Context& ctx)
+void main_loop(Context& ctx, Graphics_Pass& pass)
 {
     while (!glfwWindowShouldClose(ctx.window))
     {
         glfwPollEvents();
 
-        draw();
+        draw(ctx, pass);
     }
 }
 
@@ -38,7 +53,7 @@ int main()
         init_window(ctx);
         init_vulkan(ctx);
         create_graphics_pass(ctx, pass);
-        main_loop(ctx);
+        main_loop(ctx, pass);
         destroy_graphics_pass(ctx, pass);
         cleanup_vulkan(ctx);
         cleanup_window(ctx);
